135ff8184b55b3f311a977f898f6fb8ee8353c39
Marissa Allen <marissa@dali.dartmouth.edu>
2014-10-26 15:58:05 -0400

    Made makeBezier function

--- foldlings/SketchView.swift
+++ foldlings/SketchView.swift
@@ -103,14 +103,7 @@ class SketchView: UIView {
             pts[ctr] = touch.locationInView(self)
             if (ctr == 4)
             {
-                pts[3] = CGPointMake((pts[2].x + pts[4].x)/2.0, (pts[2].y + pts[4].y)/2.0 )// move the endpoint to the middle of the line joining the second control point of the first Bezier segment and the first control point of the second Bezier segment
-                path.moveToPoint(pts[0])
-                path.addCurveToPoint(pts[3], controlPoint1: pts[1], controlPoint2: pts[2])// add a cubic Bezier from pt[0] to pt[3], with control points pt[1] and pt[2]
-                self.setNeedsDisplay()
-                // replace points and get ready to handle the next segment
-                pts[0] = pts[3]
-                pts[1] = pts[4]
-                ctr = 1
+                makeBezier()
             }
         default:
             break
@@ -189,5 +182,16 @@ class SketchView: UIView {
 
     }
     
+    func makeBezier(){
+                pts[3] = CGPointMake((pts[2].x + pts[4].x)/2.0, (pts[2].y + pts[4].y)/2.0 )// move the endpoint to the middle of the line joining the second control point of the first Bezier segment and the first control point of the second Bezier segment
+                path.moveToPoint(pts[0])
+                path.addCurveToPoint(pts[3], controlPoint1: pts[1], controlPoint2: pts[2])// add a cubic Bezier from pt[0] to pt[3], with control points pt[1] and pt[2]
+                self.setNeedsDisplay()
+                // replace points and get ready to handle the next segment
+                pts[0] = pts[3]
+                pts[1] = pts[4]
+                ctr = 1
+
+}
 
 }
