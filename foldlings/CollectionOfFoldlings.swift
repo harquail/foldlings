17004f6dac2fd413c8318352588993bafd027aeb
Nook <nook@harquail.com>
2014-10-26 17:44:37 -0400

    collections for foldings

---
+++ foldlings/CollectionOfFoldlings.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Brian Coleman on 2014-09-04.
+//  Copyright (c) 2014 Brian Coleman. All rights reserved.
+//
+
+import UIKit
+
+class CollectionOfFoldlings: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    @IBOutlet var collectionView: UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 90, height: 90)
+//        collectionView = UICollectionView(frame: collectionView.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.registerClass(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView!)
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as CollectionViewCell
+        cell.backgroundColor = UIColor.blackColor()
+        cell.textLabel?.text = "\(indexPath.section):\(indexPath.row)"
+        cell.imageView?.image = UIImage(named: "circle")
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
