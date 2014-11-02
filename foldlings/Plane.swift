66d80dab7a8ab0021d315a137b9fc67a0ed6c708
Marissa Allen <marissa@dali.dartmouth.edu>
2014-11-01 21:02:19 -0400

    made plane class and file

---
+++ foldlings/Plane.swift
@@ -0,0 +1,14 @@
+//
+//  Plane.swift
+//  foldlings
+//
+//  Created by Marissa Allen on 11/1/14.
+//  Copyright (c) 2014 nook. All rights reserved.
+//
+
+import Foundation
+
+
+class Plane: Printable {
+    var name = "Plane"
+}
