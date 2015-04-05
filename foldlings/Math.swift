b55acbd8cd478188bb34f4164994b0351ddeadd7
Marissa Allen <marissa@dali.dartmouth.edu>
2015-04-05 16:53:30 -0400

    assert CGPathElement and switched to centroid instead of first point

--- foldlings/Math.swift
+++ foldlings/Math.swift
@@ -21,10 +21,10 @@ func getAngle(edgeA: Edge, edgeB: Edge) ->CGFloat{
 //    var a = CGPointNormalize(CGPointSubtract(findControlPoint(edgeA.path), edgeA.start))
 //    var b = CGPointNormalize(CGPointSubtract(findControlPoint(edgeB.path), edgeB.start))
     
-//    var a = CGPointNormalize(CGPointSubtract(findCentroid(edgeA.path), edgeA.start))
-//    var b = CGPointNormalize(CGPointSubtract(findCentroid(edgeB.path), edgeB.start))
-    var a = CGPointNormalize(CGPointSubtract(getFirstPoint(edgeA.path), edgeA.start))
-    var b = CGPointNormalize(CGPointSubtract(getFirstPoint(edgeB.path), edgeB.start))
+    var a = CGPointNormalize(CGPointSubtract(findCentroid(edgeA.path), edgeA.start))
+    var b = CGPointNormalize(CGPointSubtract(findCentroid(edgeB.path), edgeB.start))
+//    var a = CGPointNormalize(CGPointSubtract(getFirstPoint(edgeA.path), edgeA.start))
+//    var b = CGPointNormalize(CGPointSubtract(getFirstPoint(edgeB.path), edgeB.start))
     println("new pointa: \(getFirstPoint(edgeA.path)) \n")
     println("new pointb: \(getFirstPoint(edgeB.path)) \n")
 
