3511c6b4c8c0228c3335e7e13e4aed828d977677
Nook <nook@harquail.com>
2015-04-08 23:55:16 -0400

    merged from master

--- foldlings/Math.swift
+++ foldlings/Math.swift
@@ -21,8 +21,8 @@ func getAngle(edgeA: Edge, edgeB: Edge) ->CGFloat{
 //    var a = CGPointNormalize(CGPointSubtract(findControlPoint(edgeA.path), edgeA.start))
 //    var b = CGPointNormalize(CGPointSubtract(findControlPoint(edgeB.path), edgeB.start))
     
-   var a = CGPointNormalize(CGPointSubtract(findCentroid(edgeA.path), edgeA.start))
-   var b = CGPointNormalize(CGPointSubtract(findCentroid(edgeB.path), edgeB.start))
+    var a = CGPointNormalize(CGPointSubtract(findCentroid(edgeA.path), edgeA.start))
+    var b = CGPointNormalize(CGPointSubtract(findCentroid(edgeB.path), edgeB.start))
 //    var a = CGPointNormalize(CGPointSubtract(getFirstPoint(edgeA.path), edgeA.start))
 //    var b = CGPointNormalize(CGPointSubtract(getFirstPoint(edgeB.path), edgeB.start))
 //    println("new pointa: \(getFirstPoint(edgeA.path)) \n")
