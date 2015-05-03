fabab05ed522e61d816e52c411113ad17407cc94
harquail <nook@harquail.com>
2015-05-03 16:45:51 -0400

    no idea why this is crashing.  committing to get back to this

--- foldlings/SketchView.swift
+++ foldlings/SketchView.swift
@@ -94,13 +94,21 @@ class SketchView: UIView {
         if(gesture.state == UIGestureRecognizerState.Began){
             
             
-            if sketch.tappedFeature?.activeOption == .MoveFolds{
-                        if let children = sketch.masterFeature?.children{
-                            for child in children{
-                                if(child.boundingBox()!.contains(gesture.locationInView(self))){
+//            println(sketch.tappedFeature)
+            println("rawrBefore")
+            if let tappedF = sketch.tappedFeature{
+                
+            if let activeOpt = tappedF.activeOption {
+                println("rawrAfter")
+                if activeOpt == .MoveFolds{
+//                        if let children = sketch.masterFeature?.children{
+//                            for child in children{
+//                                if(tappedF.boundingBox()!.contains(gesture.locationInView(self))){
             
+                                    println("rawrABitAfter")
+
                                     //get the edge & nearest point to hit
-                                    let edge = child.featureEdgeAtPoint(gesture.locationInView(self))
+                                    let edge = tappedF.featureEdgeAtPoint(gesture.locationInView(self))
                                     if let e = edge{
             
                                         // keep track of change to dragged edges
@@ -111,11 +119,17 @@ class SketchView: UIView {
                                     else{
                                         println("No Edge Here...")
                                     }
+                                    println("rawrABitAfter")
+
 //                                    goodPlaceToDraw = false
-                                    return
-                                }
-                            }
-                        }
+//                                }
+//                            }
+//                        }
+                }
+                println("rawrVeryMuchAfter")
+                
+                }
+                return
             }
             
             // make a shape with touchpoint
@@ -130,7 +144,7 @@ class SketchView: UIView {
         let shape = sketch.currentFeature as! FreeForm
         // if it's been a few microseconds since we tried to add a point
         let multiplier = Float(CalculateVectorMagnitude(gesture.velocityInView(self))) * 0.5
-        if(gesture.state == UIGestureRecognizerState.Changed && (Float(shape.lastUpdated.timeIntervalSinceNow) < multiplier)){
+        if(gesture.state == UIGestureRecognizerState.Changed && (Float(shape.lastUpdated.timeIntervalSinceNow) < multiplier) && sketch.tappedFeature == nil){
             var touchPoint: CGPoint = gesture.locationInView(self)
             shape.endPoint = touchPoint
             //set the path to a curve through the points
@@ -139,7 +153,7 @@ class SketchView: UIView {
             forceRedraw()
         }
             //close the shape when the pan gesture ends
-        else if(gesture.state == UIGestureRecognizerState.Ended || gesture.state == UIGestureRecognizerState.Cancelled){
+        else if(gesture.state == UIGestureRecognizerState.Ended || gesture.state == UIGestureRecognizerState.Cancelled  && sketch.tappedFeature == nil){
             
             
             path = UIBezierPath.interpolateCGPointsWithCatmullRom(shape.interpolationPoints, closed: true, alpha: 1)
