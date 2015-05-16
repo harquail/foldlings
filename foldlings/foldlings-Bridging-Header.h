8c00ff9f00c19d2cff997d95c11e0af21da13414
harquail <nook@harquail.com>
2015-05-16 18:23:45 -0400

    merge

--- foldlings/foldlings-Bridging-Header.h
+++ foldlings/foldlings-Bridging-Header.h
@@ -17,6 +17,6 @@
 #import "ANPathIntersection.h"
 #import "PerformanceBezier/PerformanceBezier.h"
 #import <AFMInfoBanner/AFMInfoBanner.h>
-#import <UAAppReviewManager/UAAppReviewManager.h>
 //#import <AWSCore/AWSCore.h>
 #import <AWSS3/AWSS3.h>
+#import <UAAppReviewManager/UAAppReviewManager.h>
