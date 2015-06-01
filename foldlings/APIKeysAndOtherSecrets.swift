d5cde70a6b998b98755c2611427b0c7521a3f4de
harquail <nook@harquail.com>
2015-06-01 19:25:07 -0400

    actually this should not be on this branch

--- foldlings/APIKeysAndOtherSecrets.swift
+++ foldlings/APIKeysAndOtherSecrets.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-let KEY_FLURRY = "REPLACE_WITH_API_KEY"
+let KEY_FLURRY = "PWFYJP37X488YV4DKQRC"
 //TODO: Replace with real app ID before launch
-let COGNITO_IDENTITY_POOL = "REPLACE_WITH_IDENTITY_POOL"
+let COGNITO_IDENTITY_POOL = "us-east-1:4a2db9c4-171b-47cf-a6b2-7f2856c6fd06"
 let S3_BUCKET_NAME = "foldlings"
 let APP_STORE_ID = "123456"
