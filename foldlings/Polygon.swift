f8c0b93dd57ed2277a126b73bacf7227da0fc26e
harquail <nook@harquail.com>
2015-06-07 01:23:38 -0400

    placeholder

---
+++ foldlings/Polygon.swift
@@ -0,0 +1,17 @@
+//
+//  Polygon.swift
+//  foldlings
+//
+//  Created by nook on 6/7/15.
+//  Copyright (c) 2015 Marissa Allen, Nook Harquail, Tim Tregubov.  All Rights Reserved. All rights reserved.
+//
+
+import Foundation
+
+class Polygon:FoldFeature{
+
+    // the (draggable) points that define the polygon
+    var points:[CGPoint] = []
+    
+    
+}
