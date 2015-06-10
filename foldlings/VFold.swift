1d333df1ffd5a3d3123875a6aff1d47b672952ca
harquail <nook@harquail.com>
2015-06-09 21:09:05 -0400

    starting vfold, larger tap area

---
+++ foldlings/VFold.swift
@@ -0,0 +1,19 @@
+//
+//  VFold.swift
+//  foldlings
+//
+//  Created by nook on 6/9/15.
+//  Copyright (c) 2015 Marissa Allen, Nook Harquail, Tim Tregubov.  All Rights Reserved. All rights reserved.
+//
+
+import Foundation
+
+class VFold:FoldFeature{
+    // the cut that crosses the driving fold
+    var verticalCut: Edge?
+    // diagonal folds that intersect with driving fold
+    var diagonalFolds:[Edge] = []
+    
+    
+    
+}
