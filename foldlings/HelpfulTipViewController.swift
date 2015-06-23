9a612a26414aa5a399c05a64a5d0c9fd98f7b9f5
harquail <nook@harquail.com>
2015-06-23 14:12:53 -0400

    helpful tip view

---
+++ foldlings/HelpfulTipViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HelpfulTip.swift
+//  foldlings
+//
+//  Created by nook on 6/23/15.
+//  Copyright (c) 2015 Marissa Allen, Nook Harquail, Tim Tregubov.  All Rights Reserved. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HelpfulTipViewController: UIViewController{
+
+    // hide status bar
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+}
