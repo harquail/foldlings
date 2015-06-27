3692c1840d9cf371fe9e31765e3c6cc1208914be
harquail <nook@harquail.com>
2015-06-27 16:42:48 -0400

    started Color class

---
+++ foldlings/Color.swift
@@ -0,0 +1,33 @@
+//
+//  Color.swift
+//  foldlings
+//
+//  Created by nook on 6/27/15.
+//  Copyright (c) 2015 Marissa Allen, Nook Harquail, Tim Tregubov.  All Rights Reserved. All rights reserved.
+//
+
+import Foundation
+
+class Color:UIColor{
+    
+
+    class func orangePaperColor() -> UIColor{
+        return UIColor.blackColor()
+    }
+    class func bluePaperColor() -> UIColor{
+        return UIColor.blackColor()
+    }
+    class func yellowPaperColor() -> UIColor{
+        return UIColor.blackColor()
+    }
+    class func purplePaperColor() -> UIColor{
+        return UIColor.blackColor()
+    }
+    class func redPaperColor() -> UIColor{
+        return UIColor.blackColor()
+    }
+    class func greenPaperColor() -> UIColor{
+        return UIColor.blackColor()
+    }
+
+}
